a69ef38594cae02850dc6df57463b88a9409135b
jose_9722@hotmail.com <jose_9722@hotmail.com>
2016-11-21 15:32:40 +0100

    Examen

--- src/Putadon.java
+++ src/Putadon.java
@@ -11,27 +11,32 @@ import java.util.Scanner;
  * 
  */
 public class Putadon {
-	
+	static char letra = 0;
+	static String frase="";
 	public static void main(String[] args) {
 		//Variables inicialización
-		String frase="";
-		int numPos=0;
+		int numPos;
 		int numRepeticiones=0;
-		char letra=0;
+		letra=BuscaLetra();
+		EncuentraLetra(letra, frase, numRepeticiones);
 		
-		//Pedimos datos
+	}
+	//Pedimos datos
+	public static char BuscaLetra(){
 		Scanner sc=new Scanner(System.in);
 		
 		System.out.println("Introduce tu frase");
 		frase = sc.nextLine();
-		
 		System.out.println("Introduce la letra a buscar");
 		letra = sc.nextLine().charAt(0); //charAt(0) devuelve la primera letra
+		return letra;
+	}
 		
 		//Buscamos las letras
+	public static void EncuentraLetra(char letra, String frase, int numRepeticiones){
 		for(int i=0;i<frase.length();i++){
-			if(letra==frase.charAt(i-1)){
-				numRepeticiones=numRepeticiones;
+			if(letra==frase.charAt(i)){
+				numRepeticiones=numRepeticiones+1;
 				System.out.println("Letra "+letra+" encontrada en la posicion "+i);
 			}
 		}
